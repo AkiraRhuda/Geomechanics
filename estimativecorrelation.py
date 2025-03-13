45e0e710e9e80dfa0250490f9f11dc1daaa69137
Akira <akirarhuda@outlook.com>
2025-03-13 16:06:36 -0300

    0.1

--- estimativecorrelation.py
+++ estimativecorrelation.py
@@ -3,52 +3,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import read_excel
 
-# Importing Data #
-estcorDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None)
-estcorDF = estcorDF.fillna(0)
-wellinfoDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skipfooter=22,header=None,index_col=0)
+class Estimativecorrelation:
+    def __init__(self, estcorDF, wellinfoDF, tensi=np.float64, depth=np.float64, x=np.float64, y=np.float64):
+        self.tension(x, y)
+        self.grad(tensi, depth)
+        self.start(estcorDF, wellinfoDF)
+        self.plot()
 
-# Data #
-tensionDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-gradDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-totalprofDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-totalprofDF = estcorDF['prof (m)'] + wellinfoDF[2]['LÂMINA DÁGUA (m):']
+    def tension(self, x, y):
+        return 1.422 * x * y
 
-print(estcorDF)
-print(wellinfoDF)
+    def grad(self, tensi, depth):
+        return tensi / (0.1704 * (depth))
 
-rho_seawater = wellinfoDF[2][0]
-rho_region = wellinfoDF[2][1]
-water_depth = wellinfoDF[2][2]
-air_gap = wellinfoDF[2][3]
+    def start(self, estcorDF,wellinfoDF):
+        #estcorDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None)
+        estcorDF = estcorDF.fillna(0)
+        #wellinfoDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skipfooter=22,header=None,index_col=0)
 
-def tension(x,y):
-    return 1.422*x*y
+        # Data #
+        tensionDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
+        gradDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
+        totalprofDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
+        totalprofDF = estcorDF['prof (m)'] + wellinfoDF[2]['LÂMINA DÁGUA (m):']
 
-def grad(tensi,depth):
-    return tensi/(0.1704*(depth))
+        rho_seawater = wellinfoDF[2][0]
+        rho_region = wellinfoDF[2][1]
+        water_depth = wellinfoDF[2][2]
+        air_gap = wellinfoDF[2][3]
+
+        for i in range(len(estcorDF.index)):
+            if i==0:
+                tensionDF[0][i] = self.tension(water_depth, rho_seawater)
+                gradDF[0][i] = self.grad(tensionDF[0][i], totalprofDF[i])
+            else:
+                tensionDF[0][i] = tensionDF[0][i-1] + self.tension(estcorDF['ΔD (m)'][i],estcorDF['ρ (g/cm3)'][i])
+                gradDF[0][i] = self.grad(tensionDF[0][i], totalprofDF[i])
 
 
-for i in range(len(estcorDF.index)):
-    if i==0:
-        tensionDF[0][i] = tension(water_depth, rho_seawater)
-        gradDF[0][i] = grad(tensionDF[0][i], totalprofDF[i])
-    else:
-        tensionDF[0][i] = tensionDF[0][i-1] + tension(estcorDF['ΔD (m)'][i],estcorDF['ρ (g/cm3)'][i])
-        gradDF[0][i] = grad(tensionDF[0][i], totalprofDF[i])
+    def plot(self, tensionDF, estcorDF, gradDF, totalprofDF):
+        plt.plot(tensionDF, estcorDF['prof (m)'], color='red')
+        plt.xlabel('Tensão [$psi$]')
+        plt.ylabel('Profundidade [$m$]')
+        plt.title('Pressão da sobrecarga $versus$ profundidade')
+        plt.show()
 
-
-plt.plot(tensionDF, estcorDF['prof (m)'], color='red')
-plt.xlabel('Tensão [$psi$]')
-plt.ylabel('Profundidade [$m$]')
-plt.title('Pressão da sobrecarga $versus$ profundidade')
-plt.show()
-
-plt.plot(gradDF, totalprofDF, color='purple')
-plt.xlabel('Gradiente de sobrecarga [$lb/gal$]')
-plt.ylabel('Profundidade [$m$]')
-plt.title('Gradiente de sobrecarga $versus$ Profundidade')
-plt.show()
+        plt.plot(gradDF, totalprofDF, color='purple')
+        plt.xlabel('Gradiente de sobrecarga [$lb/gal$]')
+        plt.ylabel('Profundidade [$m$]')
+        plt.title('Gradiente de sobrecarga $versus$ Profundidade')
+        plt.show()
 
 """plt.plot(totalprofDF,gradDF, color='purple')
 plt.xlabel('Profundidade [$m$]')
