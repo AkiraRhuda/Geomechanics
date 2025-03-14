4425e3f84b9f7d8897376c0897cef6b96bf3f158
Akira <akirarhuda@outlook.com>
2025-03-14 01:15:23 -0300

    0.3

--- classes.py
+++ classes.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import read_excel
 
-class Estimativecorrelation:
-    def __init__(self, estcorDF, wellinfoDF):
-        self.start(estcorDF, wellinfoDF)
-        self.plot(estcorDF)
+class Overburden:
+    def __init__(self, wellDF, wellinfoDF):
+        self.start(wellDF, wellinfoDF)
+        self.plot(wellDF)
         self.tensionDF
         self.gradDF
         self.totalprofDF
@@ -19,30 +19,30 @@ class Estimativecorrelation:
     def grad(tensi, depth):
         return tensi / (0.1704 * (depth))
 
-    def start(self, estcorDF,wellinfoDF):
-        estcorDF = estcorDF.fillna(0)
+    def start(self, wellDF,wellinfoDF):
+        wellDF = wellDF.fillna(0)
         # Data #
-        self.tensionDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-        self.gradDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-        self.totalprofDF = pd.DataFrame(np.zeros(len(estcorDF.index)))
-        self.totalprofDF = estcorDF['prof (m)'] + wellinfoDF[2]['LÂMINA DÁGUA (m):']
+        self.tensionDF = pd.DataFrame(np.zeros(len(wellDF.index)))
+        self.gradDF = pd.DataFrame(np.zeros(len(wellDF.index)))
+        self.totalprofDF = pd.DataFrame(np.zeros(len(wellDF.index)))
+        self.totalprofDF = wellDF['prof (m)'] + wellinfoDF[2]['LÂMINA DÁGUA (m):']
 
         rho_seawater = wellinfoDF[2][0]
         rho_region = wellinfoDF[2][1]
         water_depth = wellinfoDF[2][2]
-        air_gap = wellinfoDF[2][3]
+        #air_gap = wellinfoDF[2][3] if wellinfoDF[2][3] is not 
 
-        for i in range(len(estcorDF.index)):
+        for i in range(len(wellDF.index)):
             if i==0:
                 self.tensionDF[0][i] = self.tension(water_depth, rho_seawater)
                 self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
             else:
-                self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(estcorDF['ΔD (m)'][i],estcorDF['ρ (g/cm3)'][i])
+                self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
                 self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
 
-    def plot(self, estcorDF):
-        plt.plot(self.tensionDF, estcorDF['prof (m)'], color='red')
+    def plot(self, wellDF):
+        plt.plot(self.tensionDF, wellDF['prof (m)'], color='red')
         plt.xlabel('Tensão [$psi$]')
         plt.ylabel('Profundidade [$m$]')
         plt.title('Pressão da sobrecarga $versus$ profundidade')
@@ -53,3 +53,34 @@ class Estimativecorrelation:
         plt.ylabel('Profundidade [$m$]')
         plt.title('Gradiente de sobrecarga $versus$ Profundidade')
         plt.show()
+        
+        
+        
+class Gardnercorrelation:
+        def __init__(self, wellDF, wellinfoDF, a, b):
+            self.wellDF, self.wellinfoDF, self.a, self.b = wellDF, wellinfoDF, a, b
+            self.calculate()
+            
+        def calculate(self):
+                self.wellDF = self.wellDF.fillna(0)
+                # Data #
+                
+                for i in range(len(self.wellDF.index)):
+                    self.wellDF['ρ (g/cm3)'] = self.a * (10**6/self.wellDF['Δt (μs/ft)'])**self.b
+                self.wellDF.replace([np.inf, -np.inf], np.nan, inplace=True)
+                return self.wellDF
+            
+"""class Belloti:
+        def __init__(self, wellDF, wellinfoDF):
+            self.wellDF, self.wellinfoDF = wellDF, wellinfoDF
+            self.calculate()
+            
+        def calculate(self):
+                self.wellDF = self.wellDF.fillna(0)
+                # Data #
+                
+                for i in range(len(self.wellDF.index)):
+                    if wellDF['Δt (μs/ft)'] > 100
+                    self.wellDF['ρ (g/cm3)'] = self.a * (10**6/self.wellDF['Δt (μs/ft)'])**self.b
+                self.wellDF.replace([np.inf, -np.inf], np.nan, inplace=True)
+                return self.wellDF"""
--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import classes
 import matplotlib.pyplot as plt
 from pandas import read_excel
 
-estcorDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None)
+# Ex 1
+
+wellDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None)
 wellinfoDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skipfooter=22,header=None,index_col=0)
 
-classes.Estimativecorrelation(estcorDF, wellinfoDF)
+classes.Overburden(wellDF, wellinfoDF)
+
+# Ex 2
+
+wellDF = pd.read_excel('source\\tabela 2.xlsx', skiprows=4, index_col=None)
+wellinfoDF = pd.read_excel('source\\tabela 2.xlsx', skipfooter=41,header=None,index_col=0)
+
+print(classes.Gardnercorrelation(wellDF, wellinfoDF, a=0.234, b=0.25).calculate())
+wellDF = classes.Gardnercorrelation(wellDF, wellinfoDF, a=0.234, b=0.25).calculate()
+classes.Overburden(wellDF, wellinfoDF)
