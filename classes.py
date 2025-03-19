6996b0d5b69dab25a5ef3274b23623d08349a383
Akira <akirarhuda@outlook.com>
2025-03-19 00:31:52 -0300

    0.7

--- classes.py
+++ classes.py
@@ -10,16 +10,22 @@ from pandas.core.methods.selectn import SelectNSeries
 
 class Overburden:
     def __init__(self, wellDF, wellinfoDF, name, water=None, unknownregion=None, sumwater=None):
-        self.start(wellDF, wellinfoDF, name, water, unknownregion, sumwater)
-        self.plot(wellDF)
+        self.wellDF = wellDF
+        self.wellinfoDF = wellinfoDF
+        self.name = name
+        self.water = water
+        self.unknownregion = unknownregion
+        self.sumwater = sumwater
         self.tensionDF = None
         self.gradDF = None
         self.totalprofDF = None
-        self.unknownregion = None
-        self.water = None
+        self.unknownregion = unknownregion
+        self.water = water
         self.rho_seawater = None
         self.rho_region = None
         self.water_depth = None
+        self.start()
+        self.plot()
 
     @staticmethod
     def tension(x, y):
@@ -29,92 +35,92 @@ class Overburden:
     def grad(tensi, depth):
         return tensi / (0.1704 * (depth))
 
-    def start(self, wellDF,wellinfoDF, name, water, unknownregion, sumwater):
+    def start(self):
         # Removing NaN entries #
-        wellDF = wellDF.fillna(0)
+        self.wellDF = self.wellDF.fillna(0)
 
         # Data and Data maniputalion #
-        self.tensionDF = pd.DataFrame(np.zeros(len(wellDF.index)))
-        self.gradDF = pd.DataFrame(np.zeros(len(wellDF.index)))
-        self.totalprofDF = pd.DataFrame(np.zeros(len(wellDF.index)))
-        if sumwater == True:
-            self.totalprofDF = wellDF['prof (m)'] + wellinfoDF[2]['LÂMINA DÁGUA (m):']
+        self.tensionDF = pd.DataFrame(np.zeros(len(self.wellDF.index)))
+        self.gradDF = pd.DataFrame(np.zeros(len(self.wellDF.index)))
+        self.totalprofDF = pd.DataFrame(np.zeros(len(self.wellDF.index)))
+        if self.sumwater == True:
+            self.totalprofDF = self.wellDF['prof (m)'] + self.wellinfoDF[2]['LÂMINA DÁGUA (m):']
         else:
-            self.totalprofDF = wellDF['prof (m)']
-        self.unknownregion = unknownregion if unknownregion is not None else None
-        self.water = water if water is not None else None
+            self.totalprofDF = self.wellDF['prof (m)']
+        self.unknownregion = self.unknownregion if self.unknownregion is not None else None
+        self.water = self.water if self.water is not None else None
 
         # Extracting constants #
-        self.rho_seawater = wellinfoDF[2][0]
-        self.rho_region = wellinfoDF[2][1]
-        self.water_depth = wellinfoDF[2][2]
+        self.rho_seawater = self.wellinfoDF[2][0]
+        self.rho_region = self.wellinfoDF[2][1]
+        self.water_depth = self.wellinfoDF[2][2]
 
         if self.unknownregion is None and self.water is not None:
-            for i in range(len(wellDF.index)):
+            for i in range(len(self.wellDF.index)):
                 if i==self.water: # ALWAYS SURFACE #
                     self.tensionDF[0][i] = self.tension(self.water_depth, self.rho_seawater)
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
                 else:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
         elif self.unknownregion is not None and self.water is None:
-            for i in range(len(wellDF.index)):
+            for i in range(len(self.wellDF.index)):
                 if i==0 and not self.unknownregion == 0:
-                    self.tensionDF[0][i] = self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
                 elif i == self.unknownregion and i == 0:
-                    self.tensionDF[0][i] = self.tension(wellDF['ΔD (m)'][i], self.rho_region)
+                    self.tensionDF[0][i] = self.tension(self.wellDF['ΔD (m)'][i], self.rho_region)
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
                 elif i == self.unknownregion:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i], self.rho_region)
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i], self.rho_region)
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
                 else:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
         elif self.unknownregion is None and self.water is None:
-            for i in range(len(wellDF.index)):
+            for i in range(len(self.wellDF.index)):
                 if i==0:
-                    self.tensionDF[0][i] = self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
                 else:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
 
         else:
-            for i in range(len(wellDF.index)):
+            for i in range(len(self.wellDF.index)):
                 if i==self.water: # ALWAYS SURFACE #s
                     self.tensionDF[0][i] = self.tension(self.water_depth, self.rho_seawater)
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
                 elif i==self.unknownregion:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i], self.rho_region)
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i], self.rho_region)
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
                 else:
-                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(wellDF['ΔD (m)'][i],wellDF['ρ (g/cm3)'][i])
+                    self.tensionDF[0][i] = self.tensionDF[0][i-1] + self.tension(self.wellDF['ΔD (m)'][i],self.wellDF['ρ (g/cm3)'][i])
                     self.gradDF[0][i] = self.grad(self.tensionDF[0][i], self.totalprofDF[i])
         self.tensionDF.columns = ['Tension']
         self.gradDF.columns = ['Overburden']
-        wellDF = pd.concat([wellDF, self.tensionDF], axis=1)
+        wellDF = pd.concat([self.wellDF, self.tensionDF], axis=1)
 
-        wellDF = pd.concat([wellDF, self.gradDF], axis=1)
-        wellDF.to_excel(f'output\\{name}.xlsx')
-        return wellDF, wellinfoDF, self.tensionDF,  self.gradDF
+        wellDF = pd.concat([self.wellDF, self.gradDF], axis=1)
+        wellDF.to_excel(f'output\\{self.name}.xlsx')
+        return self.wellDF, self.wellinfoDF, self.tensionDF,  self.gradDF, self.totalprofDF, self.water_depth
 
-    def plot(self, wellDF):
+    def plot(self):
 
         # Cleaning zero values for Tension and overburden #
         self.tensionDF['Tension'] = self.tensionDF['Tension'].loc[(self.tensionDF['Tension'] != 0)]
-        self.gradDF['Overburden'] = self.tensionDF['Tension'].loc[(self.tensionDF['Tension'] != 0)]
+        self.gradDF['Overburden'] = self.gradDF['Overburden'].loc[(self.gradDF['Overburden'] != 0)]
 
-        plt.plot(self.tensionDF, wellDF['prof (m)'], color='red')
+        plt.plot(self.tensionDF, self.wellDF['prof (m)'], color='red')
         plt.xlabel('Tensão [$psi$]')
         plt.ylabel('Profundidade [$m$]')
         plt.title('Pressão da sobrecarga $versus$ profundidade')
         plt.grid()
-        plt.ylim([0, wellDF['prof (m)'].max()])
+        plt.ylim([0, self.wellDF['prof (m)'].max()])
         plt.show()
 
         plt.plot(self.gradDF, self.totalprofDF, color='purple')
@@ -132,30 +138,48 @@ class Overburden:
         plt.show()
 
 class multiplot:
-    def __init__(self, wellDF1, tensionDF, gradDF, wellDF2, tensionDF2, gradDF2):
-        self.wellDF1 = wellDF1
+    def __init__(self, wellDF, tensionDF, gradDF, totalprofDF, water_depth, wellDF2, tensionDF2, gradDF2, totalprofDF2, water_depth2):
+        self.wellDF = wellDF
         self.tensionDF = tensionDF
         self.gradDF = gradDF
+        self.totalprofDF = totalprofDF
+        self.water_depth = water_depth
         self.wellDF2 = wellDF2
-        self.tensionDF = tensionDF2
-        self.gradDF = gradDF2
+        self.tensionDF2 = tensionDF2
+        self.gradDF2 = gradDF2
+        self.totalprofDF2 = totalprofDF2
+        self.water_depth2 = water_depth2
         self.plot()
 
 
-    def plot(self,):
+    def plot(self):
+        
+        # Cleaning zero values for Tension and overburden #
+        self.tensionDF['Tension'] = self.tensionDF['Tension'].loc[(self.tensionDF['Tension'] != 0)]
+        self.gradDF['Overburden'] = self.gradDF['Overburden'].loc[(self.gradDF['Overburden'] != 0)]
+        self.tensionDF2['Tension'] = self.tensionDF2['Tension'].loc[(self.tensionDF2['Tension'] != 0)]
+        self.gradDF2['Overburden'] = self.gradDF2['Overburden'].loc[(self.gradDF2['Overburden'] != 0)]
+
+        
         plt.plot(self.tensionDF, self.wellDF['prof (m)'], color='red', label='Well 1')
+        plt.plot(self.tensionDF2, self.wellDF2['prof (m)'], color='blue', label='Well 2')
         plt.xlabel('Tensão [$psi$]')
         plt.ylabel('Profundidade [$m$]')
         plt.title('Pressão da sobrecarga $versus$ profundidade')
         plt.grid()
         plt.legend()
-        if self.wellDF['prof (m)'].max():
+        if self.wellDF['prof (m)'].max() > self.wellDF2['prof (m)'].max():
             plt.ylim([0, self.wellDF['prof (m)'].max()])
+        else:
+            plt.ylim([0, self.wellDF2['prof (m)'].max()])
         plt.show()
 
-        plt.plot(self.gradDF, self.totalprofDF, color='purple')
+        plt.plot(self.gradDF, self.totalprofDF, color='purple', label='Well 1')
+        plt.plot(self.gradDF2, self.totalprofDF2, color='pink', label='Well 2')
         plt.axline((0, self.totalprofDF.max()), (10, self.totalprofDF.max()), color='black', ls=':',
-                   label=f'Profun máx = {self.totalprofDF.max()} $m$')
+                   label=f'Profun máx well 1 = {self.totalprofDF.max()} $m$')
+        plt.axline((0, self.totalprofDF2.max()), (10, self.totalprofDF2.max()), color='gray', ls=':',
+                   label=f'Profun máx well 2 = {self.totalprofDF2.max()} $m$')
         plt.axline((0, self.water_depth), (10, self.water_depth), color='blue', ls=':',
                    label=f'Lâmi dágua = {self.water_depth} $m$')
         plt.xlabel('Gradiente de sobrecarga [$lb/gal$]')
@@ -183,21 +207,49 @@ class Gardnercorrelation:
                 self.wellDF.replace([np.inf, -np.inf], np.nan, inplace=True)
                 return self.wellDF
             
-class Belloti:
-        def __init__(self, wellDF, wellinfoDF):
-            self.wellDF, self.wellinfoDF = wellDF, wellinfoDF
+class Bellotti:
+        def __init__(self, wellDF, wellinfoDF, dtmatrix=None, force_condition=None):
+            self.wellDF, self.wellinfoDF, self.dtmatrix = wellDF, wellinfoDF, dtmatrix
+            self.force_condition = force_condition
             self.calculate()
             
         def calculate(self):
-                self.wellDF = self.wellDF.fillna(0)
-                # Data #
-                
+            self.wellDF = self.wellDF.fillna(0)
+
+            # Data #
+            if self.force_condition is None or False:
                 for i in range(len(self.wellDF.index)):
                     if self.wellDF['Δt (μs/ft)'][i] > 100:
-                        self.wellDF['ρ (g/cm3)'][i] = 0
+                        self.wellDF['ρ (g/cm3)'] = 2.75 - 2.11*(self.wellDF['Δt (μs/ft)']-self.dtmatrix)/(self.wellDF['Δt (μs/ft)']+200)
                     elif self.wellDF['Δt (μs/ft)'][i] < 100 and self.wellDF['Δt (μs/ft)'][i] != 0:
-                        self.wellDF['ρ (g/cm3)'][i] = 3.28 - self.wellDF['Δt (μs/ft)'][i]/88.95
-                self.wellDF.replace([np.inf, -np.inf], np.nan, inplace=True)
-                return self.wellDF
+                        self.wellDF['ρ (g/cm3)'] = 3.28 - self.wellDF['Δt (μs/ft)']/88.95
+            elif self.force_condition == 'consolidated':
+                for i in range(len(self.wellDF.index)):
+                    self.wellDF['ρ (g/cm3)'] = 3.28 - self.wellDF['Δt (μs/ft)']/88.95
+            elif self.force_condition == 'unconsolidated':
+                self.wellDF['ρ (g/cm3)'] = 2.75 - 2.11*(self.wellDF['Δt (μs/ft)']-self.dtmatrix)/(self.wellDF['Δt (μs/ft)']+200)
+            else:
+                raise Exception(f"The given condition does't exist")
+                        
+            self.wellDF.replace([np.inf, -np.inf], np.nan, inplace=True)
+            return self.wellDF
 
-        #def plot(self):
+class Bourgoyne:
+    def __init__(self, wellDF, wellinfoDF):
+            self.wellDF, self.wellinfoDF = wellDF, wellinfoDF
+            self.rho_seawater = None
+            self.rho_region = None
+            self.start()
+            
+    def start(self):
+        # Extracting constants #
+        self.rho_seawater = self.wellinfoDF[1][0]
+        self.rho_region = self.wellinfoDF[1][1]
+        
+        plt.plot(self.wellDF['Porosidade'], self.wellDF['prof (m)'], color='red')
+        plt.xlabel('Porosidade')
+        plt.ylabel('Profundidade [$m$]')
+        plt.title('Porosidade versus profundidade')
+        plt.grid()
+        plt.ylim([0, self.wellDF['prof (m)'].max()])
+        plt.show()
--- main.py
+++ main.py
@@ -3,21 +3,42 @@ import pandas as pd
 import classes
 import matplotlib.pyplot as plt
 from pandas import read_excel
-
+"""
 # Ex 1
-
-wellDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None)
-wellinfoDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skipfooter=22,header=None,index_col=0)
+wellDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skiprows=4, index_col=None, decimal=',')
+wellinfoDF = pd.read_excel('source\\correlacoes_estimativa_tensao_sobrecarga.xlsx', skipfooter=22,header=None,index_col=0, decimal=',')
 
 classes.Overburden(wellDF, wellinfoDF,name='1', water=0, sumwater=True)
 
 # Ex 2
 
-wellDF = pd.read_excel('source\\tabela 2.xlsx', skiprows=4, index_col=None)
-wellinfoDF = pd.read_excel('source\\tabela 2.xlsx', skipfooter=41,header=None,index_col=0)
+wellDF = pd.read_excel('source\\tabela 2.xlsx', skiprows=4, index_col=None, decimal=',')
+wellinfoDF = pd.read_excel('source\\tabela 2.xlsx', skipfooter=41,header=None,index_col=0, decimal=',')
 
-wellDF = classes.Gardnercorrelation(wellDF, wellinfoDF, a=0.234, b=0.25).calculate()
-classes.Overburden(wellDF, wellinfoDF,name='2', water=0, unknownregion=1)
 
-wellDF = classes.Belloti(wellDF, wellinfoDF).calculate()
-classes.Overburden(wellDF, wellinfoDF,name='2.1')
+wellDF1 = classes.Gardnercorrelation(wellDF, wellinfoDF, a=0.234, b=0.25).calculate() 
+
+classes.Overburden(wellDF1, wellinfoDF,name='2', water=0, unknownregion=1)
+
+wellDF2 = classes.Belloti(wellDF, wellinfoDF, force_condition='consolidated').calculate()
+classes.Overburden(wellDF2, wellinfoDF,name='2.1', water=0, unknownregion=1)
+
+
+# Multiplot #
+
+wellDF1 = classes.Gardnercorrelation(wellDF, wellinfoDF, a=0.234, b=0.25).calculate() 
+
+wellDF1, wellinfoDF, tensionDF,  gradDF, totalprofDF, water_depth = classes.Overburden(wellDF1, wellinfoDF,name='2', water=0, unknownregion=1).start()
+
+wellDF2 = classes.Belloti(wellDF, wellinfoDF, force_condition='consolidated').calculate()
+wellDF2, wellinfoDF2, tensionDF2,  gradDF2, totalprofDF2, water_depth2 = classes.Overburden(wellDF2, wellinfoDF,name='2.1', water=0, unknownregion=1).start()
+
+#classes.multiplot(wellDF, tensionDF,  gradDF, totalprofDF, water_depth, wellDF2, tensionDF2,  gradDF2, totalprofDF2, water_depth2)
+classes.multiplot(wellDF1, tensionDF,  gradDF, totalprofDF, water_depth, wellDF2, tensionDF2,  gradDF2, totalprofDF2, water_depth2)
+"""
+
+# Ex 3
+
+wellDF = pd.read_excel('source\\tabela 3.xlsx', skiprows=4, index_col=None, decimal=',')
+wellinfoDF = pd.read_excel('source\\tabela 3.xlsx', skipfooter=23,header=None,index_col=0, decimal=',')
+classes.Bourgoyne(wellDF, wellinfoDF)
